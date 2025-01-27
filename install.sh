5ca8dd85b3b383a2d2e9c4b3bf8c1e4e8c62b598
Sellenix <info@netslim.nl>
2025-01-27 23:54:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,14 +17,23 @@ then
 fi
 
 # Installeer benodigde pakketten
-apt install -y npm nginx
+apt install -y npm nginx mysql-server
+
+# MySQL setup
+mysql -e "CREATE DATABASE IF NOT EXISTS sellenix_db;"
+mysql -e "CREATE USER IF NOT EXISTS 'sellenix_user'@'localhost' IDENTIFIED BY 'yigitemirK2016@@';"
+mysql -e "GRANT ALL PRIVILEGES ON sellenix_db.* TO 'sellenix_user'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
 
 # Kloon de repository
-git clone https://github.com/uw-repo-url/sellenix.git /var/www/sellenix
+git clone https://github.com/Sellenix/sellenixapp.git /var/www/sellenix
 cd /var/www/sellenix
 
+# Update package.json om date-fns versie te corrigeren
+sed -i 's/"date-fns": "4.1.0"/"date-fns": "^2.30.0"/' package.json
+
 # Installeer dependencies
-npm install
+npm install --legacy-peer-deps
 
 # Maak .env bestand aan
 cat > .env << EOL
@@ -58,6 +67,17 @@ cat > /etc/nginx/sites-available/sellenix << EOL
 server {
     listen 80;
     server_name sellenix.com www.sellenix.com;
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name sellenix.com www.sellenix.com;
+
+    ssl_certificate /etc/letsencrypt/live/sellenix.com/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/sellenix.com/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 
     location / {
         proxy_pass http://localhost:3000;
@@ -67,11 +87,20 @@ server {
         proxy_set_header Host \$host;
         proxy_cache_bypass \$http_upgrade;
     }
+
+    access_log /var/log/nginx/sellenix_access.log;
+    error_log /var/log/nginx/sellenix_error.log;
 }
 EOL
 
+# Verwijder de standaard Nginx configuratie
+rm /etc/nginx/sites-enabled/default
+
+# Activeer de nieuwe configuratie
 ln -s /etc/nginx/sites-available/sellenix /etc/nginx/sites-enabled/
+
+# Test Nginx configuratie en herstart
 nginx -t && systemctl restart nginx
 
-echo "Installatie voltooid! Ga naar http://sellenix.com/install om de setup te voltooien."
+echo "Installatie voltooid! Ga naar https://sellenix.com/install om de setup te voltooien."
 
